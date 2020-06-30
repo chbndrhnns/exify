c60b9fd18a975d8e92d43eac570d2d4f8113cb37
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-06-30 10:06:11 +0200

    (feat) Run writer after analyzer

--- exify/__main__.py
+++ exify/__main__.py
@@ -1,12 +1,14 @@
 import asyncio
 from collections import defaultdict
-from typing import List
+from pathlib import Path
+from typing import List, MutableMapping
 
 from loguru import logger
 
 from exify.analyzer.whatsapp_analyzer import WhatsappFileAnalyzer
 from exify.models import ExifySettings, FileItem
 from exify.settings import get_settings
+from exify.writer.whatsapp_writer import WhatsappTimestampWriter
 
 
 def _expand_to_absolute_path(file):
@@ -18,23 +20,28 @@ def _expand_to_absolute_path(file):
 async def run(settings: ExifySettings):
     src = settings.base_dir
     logger.info(f'Running on {src}...')
+    logger.info(settings)
 
     if files := [x for x in src.iterdir() if x.is_file()]:
         results = await analyze_files(files, settings)
-        await analyze_results(results)
+        not_ok = await analyze_results(results)
+
+    for item in not_ok:
+        await WhatsappTimestampWriter(item).write_exif_data()
 
 
-async def analyze_results(items: List[FileItem]):
+async def analyze_results(items: MutableMapping[Path, FileItem]) -> List[FileItem]:
     ok = []
     not_ok = []
     for item in items.values():
-        if any([res for res in item.results]):
+        if not item.results.exif_timestamp_exists:
             not_ok.append(item)
         else:
             ok.append(item)
 
     logger.info(f'OK: {len(ok)}')
     logger.info(f'NOT OK: {len(not_ok)}')
+    return not_ok
 
 
 async def analyze_files(files, settings):
